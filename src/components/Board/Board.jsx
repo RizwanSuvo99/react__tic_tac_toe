72e53f4427143cc68f342d4761162ab6e20db1a7
RizwanSuvo99 <rizwanshuvo@gmail.com>
2024-01-29 13:34:02 +0600

    game implemented done

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,22 +1,51 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import Square from "../Square/Square";
+import { calculateWinner } from "../../utils/calculateWinner";
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player " + (xIsNext ? "X" : "O");
+  }
+
+  const handlClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = [...squares];
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares([...nextSquares]);
+    setXIsNext(!xIsNext);
+  };
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div>{status}</div>
+      <div className="flex">
+        <Square value={squares[0]} handleSquareClick={() => handlClick(0)} />
+        <Square value={squares[1]} handleSquareClick={() => handlClick(1)} />
+        <Square value={squares[2]} handleSquareClick={() => handlClick(2)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square /> 
+      <div className="flex">
+        <Square value={squares[3]} handleSquareClick={() => handlClick(3)} />
+        <Square value={squares[4]} handleSquareClick={() => handlClick(4)} />
+        <Square value={squares[5]} handleSquareClick={() => handlClick(5)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={squares[6]} handleSquareClick={() => handlClick(6)} />
+        <Square value={squares[7]} handleSquareClick={() => handlClick(7)} />
+        <Square value={squares[8]} handleSquareClick={() => handlClick(8)} />
       </div>
     </>
   );
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,9 +1,13 @@
-export default function Square() {
+/* eslint-disable react/prop-types */
+export default function Square({ value,handleSquareClick }) {
   return (
     <>
-      <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-        X
+      <button
+        onClick={handleSquareClick}
+        className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      >
+        {value}
       </button>
     </>
-  );
+  ); 
 }
